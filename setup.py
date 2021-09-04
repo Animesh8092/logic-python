727dd676e7586f088f593393d8ae25fd4dce4093
Animesh Biswas <77546785+Animesh8092@users.noreply.github.com>
2021-09-04 23:14:32 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setup(
     license="MIT",
     classifiers=[
         "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.9",
